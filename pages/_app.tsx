4afbc03947ba95daf3cc7b88762cc584bd1849e0
István Korompai <istvan.korompai@apple.com>
2023-03-16 21:43:53 +0000

    fix: strip out Next fluff, move styling to mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Paper from "@mui/material/Paper";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+import theme from "@/styles/muiTheme";
+import { ThemeProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Paper elevation={0} sx={{ minHeight: "100vh", position: "relative" }}>
+          <Component {...pageProps} />
+        </Paper>
+      </ThemeProvider>
+    </>
+  );
 }
